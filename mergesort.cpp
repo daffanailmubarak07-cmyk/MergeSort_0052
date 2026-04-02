500b8818fb922f0100a5f9d8e1f9c7e82c884d95
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-04-02 18:04:16 +0700

    Refactor mergesort.cpp for improved readability

--- mergesort.cpp
+++ mergesort.cpp
@@ -43,4 +43,23 @@ void mergeSort(int low, int high)
         return; // step 1.a
     }
 
-    
+    int mid = (low + high) / 2; // step 2
+
+    // step 3 (rekursi)
+    mergeSort(low, mid);       // step 3.a
+    mergeSort(mid + 1, high);  // step 3.b
+
+    // step 4
+    int i = low;       // step 4.a
+    int j = mid + 1;   // step 4.b
+    int k = low;       // step 4.c
+
+    while (i <= mid && j <= high) // step 4.d
+    {
+        if (arr[i] <= arr[j]) // step 4.d.i
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+       
