f2cf19aa7d78a2f2514d3ab772b7ce4e33e0357f
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-04-02 18:07:48 +0700

    Implement output function for sorted array display
    Added output function to display sorted array.

--- mergesort.cpp
+++ mergesort.cpp
@@ -89,4 +89,11 @@ for (int x = low; x <= high; x++)
     }
 }
 
- 
+   void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
